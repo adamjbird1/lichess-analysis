459e18cc64d5e0d8caac7d4eba13a14c212accea
Adam Bird <adamjbird1@gmail.com>
2023-03-29 23:40:12 +0100

    feat: Fetch date of current game and request from chess.com API

--- src/open_lichess.js
+++ src/open_lichess.js
@@ -1,74 +1,68 @@
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+chrome.tabs.query({
+    active: true,
+    currentWindow: true
+}, function(tabs) {
     const currentUrl = tabs[0].url;
-    
     // Check if the URL is a chess.com game
     if (currentUrl.startsWith("https://www.chess.com/game/live/")) {
-
         chrome.scripting.executeScript({
-            target: {tabId: tabs[0].id},
+            target: {
+                tabId: tabs[0].id
+            },
             func: function() {
                 var met = document.querySelector('meta[property="og:image"]');
                 return met.content;
-            }            
+            }
         }, function(result) {
             var username = result[0].result.split("/").slice(-2)[0];
             findGame(username, currentUrl)
                 .then(pgn => {
                     if (pgn) {
                         openLichess(pgn);
-                    }
-                    else {
+                    } else {
                         alert("Couldn't find game!");
                     }
                 })
         });
-    }
-    else {
+    } else {
         alert("Not a valid chess.com game page.")
     }
 });
 
-async function findGame(username, currentUrl) {
-    const date = new Date();
-    let [m, y] = [date.getMonth() + 1, date.getFullYear()];
-    let attempts = 0;
-    
-    while (attempts < 24) {
-        console.log(attempts);
-        //API expects YYYY/MM format
-        const apiUrl = `https://api.chess.com/pub/player/${username}/games/${y}/${m < 10 ? '0' + m : m}`;
-        console.log(apiUrl);
-        
-        const response = await fetch(apiUrl);
+async function getDate(gameID) {
+    try {
+        const url = `https://www.chess.com/callback/live/game/${gameID}`;
+        const response = await fetch(url);
         const data = await response.json();
-        
-        const games = data.games;
-        const selectedGame = games.find(game => game.url === currentUrl);
-        
-        if (selectedGame) {
-            return selectedGame.pgn;
-        }
-
-        // Decrement month and year
-        m--;
-        if (m === 0) {
-            m = 12;
-            y--;
-        }
-        
-        attempts++;
+        return data.game.pgnHeaders.Date;
+    } catch (error) {
+        console.error(`Error fetching moves for game ${gameID}: ${error}`);
+        return null;
     }
-    
-    console.log('Reached maximum number of attempts');
-    return null;
 }
 
+async function findGame(username, currentUrl) {
+    const gameID = currentUrl.split("/").pop();
+
+    const dateStr = await getDate(gameID);
+    const [y, m] = dateStr.split(".").slice(0, 2);
+
+    const apiUrl = `https://api.chess.com/pub/player/${username}/games/${y}/${m}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    const selectedGame = data.games.find(game => game.url === currentUrl);
+    if (selectedGame) {
+        return selectedGame.pgn;
+    }
+    return null;
+}
 
 function openLichess(pgn) {
     fetch('https://lichess.org/api/import', {
         method: 'POST',
         headers: {
-            'Content-Type' : 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
         body: new URLSearchParams({
             pgn: pgn
@@ -83,4 +77,4 @@ function openLichess(pgn) {
         chrome.tabs.create({ url: data.url });
     })
 }
-  
+
